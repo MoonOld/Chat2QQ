68a8d74c01c7fee90baedd29f173de1f270ab456
MoonOld <moonold.h@gmail.com>
2023-03-06 00:36:29 +0800

    a simple demo todo list: model change system option context

--- src/main/java/Moonold/Main.java
+++ src/main/java/Moonold/Main.java
@@ -1,7 +1,37 @@
 package Moonold;
+import com.unfbx.chatgpt.OpenAiClient;
+import com.unfbx.chatgpt.entity.completions.CompletionResponse;
+import com.unfbx.chatgpt.interceptor.OpenAILogger;
+import okhttp3.logging.HttpLoggingInterceptor;
+
+import java.net.InetSocketAddress;
+import java.net.Proxy;
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //配置api keys
+        //代理可以为null
+        Scanner sc = new Scanner(System.in);
+        String APIKey = "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";
+
+        Proxy proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress("localhost", 7890));
+        //日志输出可以不添加
+        HttpLoggingInterceptor httpLoggingInterceptor = new HttpLoggingInterceptor(new OpenAILogger());
+        httpLoggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+//        OpenAiClient openAiClient = new OpenAiClient("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",60,60,60);
+//        OpenAiClient openAiClient = new OpenAiClient("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",60,60,60,null);
+//        OpenAiClient openAiClient = new OpenAiClient("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");
+        OpenAiClient openAiClient = OpenAiClient.builder()
+                .apiKey(APIKey)
+                .connectTimeout(50)
+                .writeTimeout(50)
+                .readTimeout(50)
+                .interceptor(Arrays.asList(httpLoggingInterceptor))
+                .proxy(proxy)
+                .build();
+        CompletionResponse completions = openAiClient.completions("我想申请转专业，从计算机专业转到会计学专业，帮我完成一份两百字左右的申请书");
+        Arrays.stream(completions.getChoices()).forEach(System.out::println);
     }
 }
